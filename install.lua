d887db812ccb922a6b7f516c8abfa44f82c1ed52
Hambat E. <hambateverett@gmail.com>
2026-05-21 22:19:45 -0700

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,115 +1,12 @@
 term.clear()
-local spinnerChars = {"|", "/", "-", "\\"}
-local ender = 1e38
-local finished = 0
 
-for i = 1, ender do
-    term.clear()
-    term.setCursorPos(2, 2)
-    local index = ((i - 1) % #spinnerChars) + 1
-    term.write(spinnerChars[index])
-    sleep(0.1)
-    if finished == 0 then
-        term.setCursorPos(1,4)
-        print("menu.lua: Unfinished")
-        print("eggshell.lua: Unfinished")
-        print("egdia.lua: Unfinished")
-        print("assets/sat.nfp: Unfinished")
-        print("assets/sat2.nfp: Unfinished")
-        print("eggshells/help.lua: Unfinished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 1 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Unfinished")
-        print("egdia.lua: Unfinished")
-        print("assets/sat.nfp: Unfinished")
-        print("assets/sat2.nfp: Unfinished")
-        print("eggshells/help.lua: Unfinished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 2 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Unfinished")
-        print("assets/sat.nfp: Unfinished")
-        print("assets/sat2.nfp: Unfinished")
-        print("eggshells/help.lua: Unfinished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 3 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Finished")
-        print("assets/sat.nfp: Unfinished")
-        print("assets/sat2.nfp: Unfinished")
-        print("eggshells/help.lua: Unfinished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 4 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Finished")
-        print("assets/sat.nfp: Finished")
-        print("assets/sat2.nfp: Unfinished")
-        print("eggshells/help.lua: Unfinished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 5 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Finished")
-        print("assets/sat.nfp: Finished")
-        print("assets/sat2.nfp: Finished")
-        print("eggshells/help.lua: Unfinished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 6 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Finished")
-        print("assets/sat.nfp: Finished")
-        print("assets/sat2.nfp: Finished")
-        print("eggshells/help.lua: Finished")
-        print("eggshells/time.lua: Unfinished")
-        print("startup.lua: Unfinished")
-    elseif finished == 7 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Finished")
-        print("assets/sat.nfp: Finished")
-        print("assets/sat2.nfp: Finished")
-        print("eggshells/help.lua: Finished")
-        print("eggshells/time.lua: Finished")
-        print("startup.lua: Unfinished")
-    elseif finished == 8 then
-        print("menu.lua: Finished")
-        print("eggshell.lua: Finished")
-        print("egdia.lua: Finished")
-        print("assets/sat.nfp: Finished")
-        print("assets/sat2.nfp: Finished")
-        print("eggshells/help.lua: Finished")
-        print("eggshells/time.lua: Finished")
-        print("startup.lua: Finished")
-        ender = i + 1
-    end
-end
-
-shell.run("rm egde")
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/menu.lua")
-finished = 1
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/eggshell.lua")
-finished = 2
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/egdia.lua")
-finished = 3
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/assets/sat.nfp")
-finished = 4
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/assets/sat2.nfp")
-finished = 5
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/eggshells/help.lua")
-finished = 6
 shell.run("wget https://github.com/HambatEverett/EGDE-Mobile/raw/refs/heads/main/egde/eggshells/time.lua")
-finished = 7
 shell.run("wget")
-finished = 8
 sleep(1)
+os.reboot()
