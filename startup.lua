b9e4f86900d34347f775a0e908f1f95536a3a7fc
Hambat E. <hambateverett@gmail.com>
2026-05-25 04:29:52 -0700

    Add user settings definition for username

--- startup.lua
+++ startup.lua
@@ -1,1 +1,9 @@
+if not settings.get("username") then
+  settings.define("username", {
+      description = "The user's name",
+      default = "New User",
+      type = "string",
+  })
+end
+
 shell.run("egde/menu.lua")
