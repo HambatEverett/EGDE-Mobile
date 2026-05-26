f6af32f35d6ce718e5c19ffb75fe86981a46130e
Hambat E. <hambateverett@gmail.com>
2026-05-25 21:31:09 -0700

    Update settings.lua

--- egde/settings.lua
+++ egde/settings.lua
@@ -4,6 +4,7 @@ local event, button, x, y
 local screen = "main"
 term.setCursorPos(1,1)
 local btn1 = paintutils.loadImage("egde/assets/button1.nfp")
+local colorpicker = paintutils.loadImage("egde/assets/colorpicker.nfp")
 
 function organs()
   while true do
@@ -13,6 +14,9 @@ function organs()
     if screen == "main" then
       if x >= 2 and x <= 25 and y >= 2 and y <= 6 then screen = "updateask" end
       if x >= 2 and x <= 25 and y >= 8 and y <= 13 then screen = "changename" end
+      if x >= 2 and x <= 25 and y >= 14 and y <= 19 then screen = "changetc" end
+    elseif screen == "changetc" then
+      if
     end
   end
 end
@@ -44,6 +48,16 @@ function display()
       term.write("Select Username")
       term.setTextColor(colors.white)
       term.setBackgroundColor(colors.black)
+
+      -- change topbar color
+      term.setCursorPos(((tx/2)-11),14)
+      paintutils.drawImage(btn1, term.getCursorPos())
+      term.setCursorPos(((tx/2)-7),16)
+      term.setTextColor(colors.black)
+      term.setBackgroundColor(colors.lightGray)
+      term.write("Set Topbar Color")
+      term.setTextColor(colors.white)
+      term.setBackgroundColor(colors.black)
     elseif screen == "updateask" then
       term.setCursorPos((tx/2)-6, ty/2)
       term.write("Are you sure?")
@@ -66,6 +80,76 @@ function display()
       settings.set("username", input)
       settings.save(".settings")
       screen = "main"
+    elseif screen == "changetc" then
+      -- colorpicker
+      term.clear()
+      term.setCursorPos((tx/2)-3, ty/2)
+      local input = io.read():lower()
+      if input == "white" then
+        settings.set("topcolor", 0)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "orange" then
+        settings.set("topcolor", 2)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "magenta" then
+        settings.set("topcolor", 4)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "light blue" then
+        settings.set("topcolor", 8)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "yellow" then
+        settings.set("topcolor", 16)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "lime" then
+        settings.set("topcolor", 32)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "pink" then
+        settings.set("topcolor", 64)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "gray" then
+        settings.set("topcolor", 128)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "light gray" then
+        settings.set("topcolor", 256)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "cyan" then
+        settings.set("topcolor", 512)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "purple" then
+        settings.set("topcolor", 1024)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "blue" then
+        settings.set("topcolor", 2048)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "brown" then
+        settings.set("topcolor", 4096)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "green" then
+        settings.set("topcolor", 8192)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "red" then
+        settings.set("topcolor", 16374)
+        settings.save(".settings")
+        screen = "main"
+      elseif input == "black" then
+        settings.set("topcolor", 32768)
+        settings.save(".settings")
+        screen = "main"
+      end
     end
   end
 end
